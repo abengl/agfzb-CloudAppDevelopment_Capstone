1fe715e5958dceb725330875b97a2dde387cb339
abengl <alebendigl@gmail.com>
2023-08-01 12:06:40 -0400

    Finish part 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,17 +9,19 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    # path index 
+    path(route='', view=views.get_dealerships, name='index'),
     # path for about view
-
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
+    #path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
