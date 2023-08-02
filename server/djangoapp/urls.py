b53213d6c4289add49bc99287acb693b91f91ccc
abengl <alebendigl@gmail.com>
2023-08-02 13:15:51 -0400

    Lab 3.1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     # name the URL
 
     # path index 
-    path(route='', view=views.get_dealerships, name='index'),
+    #path(route='', view=views.get_dealerships, name='index'),
     
     # path for about view
-    path(route='about/', view=views.about, name='about'),
+    path('about/', view=views.about, name='about'),
     
     # path for contact us view
-    path(route='contact/', view=views.contact, name='contact'),
+    path('contact/', view=views.contact, name='contact'),
     
     # path for registration
     path('registration/', views.registration_request, name='registration'),
@@ -27,10 +27,13 @@ urlpatterns = [
     # path for logout
     path('logout/', views.logout_request, name='logout'),
 
-    #path(route='', view=views.get_dealerships, name='index'),
+    #path for index
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
