aabaac47313382e5fe7e59774befdb0f236f08a0
abengl <alebendigl@gmail.com>
2023-08-02 10:34:57 -0400

    Updates

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+#from .models import CarModel, CarMake, CarDealer, DealerReview, ReviewPost
+#from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, get_dealer_reviews_from_cf,post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -91,11 +91,7 @@ def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-    context = {}
-    url = "https://53c4f092.eu-gb.apigw.appdomain.cloud/api/dealerships"
-    dealerships = get_dealers_from_cf(url)
-    context["dealership_list"] = dealerships
-    return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
@@ -104,4 +100,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
